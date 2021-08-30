bc29740069f91b543176e027c30ce449088d9b26
SebasUni <81190314+SebasUni@users.noreply.github.com>
2021-08-30 18:24:19 -0500

    SolicitudTrasporteCli

---
+++ GestinTransporteCarga/app/src/main/java/unipiloto/edu/co/MainActivity.java
@@ -0,0 +1,27 @@
+package unipiloto.edu.co;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void CrearUsuario(View v){
+        Intent intent = new Intent(this,CrearUsuario.class);
+        startActivity(intent);
+
+    }
+    public void Login(View v){
+        Intent intent = new Intent(this,InicioCliente.class);
+        startActivity(intent);
+
+    }
+
+}
